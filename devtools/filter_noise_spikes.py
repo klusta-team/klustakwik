385f4ed676a9001cb7f70a5ce6eb35d4a88eded0
thesamovar <dg.github@thesamovar.net>
2014-12-03 19:22:04 +0000

    Speedup using pandas

--- devtools/filter_noise_spikes.py
+++ devtools/filter_noise_spikes.py
@@ -28,9 +28,42 @@ This file will correctly correspond to your original .fet file.
 '''
 from numpy import *
 import sys
-
+import time
+try:
+    # much faster than numpy's loadtxt and savetxt
+    import pandas
+    def loadtxt(fname, skiprows=0, dtype=int):
+        return pandas.read_csv(fname, sep=' ', skiprows=skiprows, dtype=dtype, header=None).values
+    def savetxt(fname, arr, dtype=int, header=None, comments=None):
+        df = pandas.DataFrame(arr)
+        with open(fname, 'w') as f:
+            if header is not None:
+                f.write(header+'\n')
+            df.to_csv(f, sep=' ', header=False, index=False)
+except ImportError:
+    # slightly faster than numpy's loadtxt
+    def loadtxt(fname, skiprows=0, dtype=int):
+        with open(fname, 'r') as f:
+            for _ in xrange(skiprows):
+                f.readline()
+            count = 0
+            for line in f:
+                if count==0:
+                    numdims = len(line.split(' '))
+                count += 1
+        out = zeros((count, numdims), dtype=dtype)
+        with open(fname, 'r') as f:
+            for _ in xrange(skiprows):
+                f.readline()
+            i = 0
+            for line in f:
+                x = array(line.split(' '), dtype=dtype)
+                out[i, :] = x
+                i += 1
+        return out
 
 def filter_spikes(fname, max_unmasked):
+    start = time.time()
     fname = fname.replace('.fet.', '?')
     basefname, shanknum = fname.split('?')
     
@@ -58,6 +91,7 @@ def filter_spikes(fname, max_unmasked):
     print 'Number of spikes = %d, number of features = %d' % (numspikes, numfeatures)
     print 'Number of spikes removed = %d (%.2f %%)' % (num_removed, num_removed*100.0/numspikes)
     print 'Number of spikes kept = %d (%.2f %%)' % (num_kept, num_kept*100.0/numspikes)
+    print 'Time taken: %.2f s' % (time.time()-start)
 
         
 def convert_clu(fname):
